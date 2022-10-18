27acae4af6f4459515db8eb6a3727762d126f41c
Alex17966 <113025851+Alex17966@users.noreply.github.com>
2022-10-18 15:42:09 +0300

    Теперь наследование от класса Shape

--- lessons_1/lesson2.py
+++ lessons_1/lesson2.py
@@ -1,57 +1,74 @@
 import math
 
 
-class Circle:
-    """
-    Приходят координаты(2 точки):
-    а - центр, b - точка на окружности
-    метод side_long вычисляет расстояние между точками(радиус)
-    """
+class Shape:
+    @staticmethod
+    def side_long(dot1, dot2):
+        return math.sqrt((dot1[0] - dot2[0]) ** 2
+                         + (dot1[1] - dot2[1]) ** 2)
 
+
+class Circle(Shape):
     def __init__(self, a, b):
-        def side_long(dot1, dot2):
-            return math.sqrt((dot1[0] - dot2[0]) ** 2
-                             + (dot1[1] - dot2[1]) ** 2)
-
         self.a = a
         self.b = b
-        self.ab = side_long(a, b)
+        self.ab = self.side_long(a, b)
 
     def perimeter(self):
-        return round(2 * 3.14 * self.ab, 2)
+        return f'Perimeter is {round(2 * 3.14 * self.ab, 2)}'
 
     def area(self):
-        return round(3.14 * self.ab ** 2, 2)
+        return f'Area is {round(3.14 * self.ab ** 2, 2)}'
 
 
-class Triangle(Circle):
-    """
-    ПОМОГИТЕ ПОЖАЛУЙСТА! Здесь ошибка в конструкторе
-    1) Как мне правильно унаследавать класс Треугольник от класса Круг, что бы добавить точку "с" и получить длинны
-    3 сторон (ab, bc, ac). У меня Пайчарм не видит метод "side_long" в классе Треугольник. Что я делаю не так?
-
-    2) Затем я собираюсь унаследовать класс Прямоугольник от класса Круг, добавив при этом точки "с", "d" и вычислив
-    длинны сторон (ab, bc, cd, ad). Это вообще правильный подход? Или нужно было создать некий абстрактный класс Shape,
-    а от него наследовать 3 класса - Круг, Треугольник, Прямоугольник?
-    """
+class Triangle(Shape):
     def __init__(self, a, b, c):
-        super().__init__(self, c)
-            self.ac = side_long(a, c)
+        self.a = a
+        self.b = b
+        self.c = c
+        self.ab = self.side_long(a, b)
+        self.bc = self.side_long(b, c)
+        self.ac = self.side_long(a, c)
 
     def perimeter(self):
-        return round(self.ab + self.bc + self.ac, 2)
+        return f'Perimeter is {round(self.ab + self.bc + self.ac, 2)}'
 
     def area(self):
         semi_perimeter = (self.ab + self.bc + self.ac) / 2
-        return round(math.sqrt(semi_perimeter *
-                         (semi_perimeter - self.ab) *
-                         (semi_perimeter - self.bc) *
-                         (semi_perimeter - self.ac)), 2)
+        area = round(math.sqrt(semi_perimeter *
+                               (semi_perimeter - self.ab) *
+                               (semi_perimeter - self.bc) *
+                               (semi_perimeter - self.ac)), 2)
+        return f'Area is {area}'
+
+
+class Rectangle(Shape):
+    def __init__(self, a, b, c, d):
+        self.a = a
+        self.b = b
+        self.c = c
+        self.d = d
+        self.ab = self.side_long(a, b)
+        self.bc = self.side_long(b, c)
+
+    def perimeter(self):
+        return f'Perimeter is {round((self.ab + self.bc) * 2, 2)}'
+
+    def area(self):
+        return f'Area is {round(self.ab * self.bc)}'
 
 
 circle = Circle((1, 1), (1, 2))
+print('Circle created')
 print(circle.perimeter())
 print(circle.area())
-# triangle1 = Triangle((0, 0), (1, 0), (0, 1))
-# print(triangle1.perimeter())
-# print(triangle1.area())
+
+triangle = Triangle((0, 0), (1, 0), (0, 1))
+print('\nTriangle created')
+print(triangle.perimeter())
+print(triangle.area())
+
+rectangle = Rectangle((0, 0), (0, 1), (2, 1), (2, 0))
+print('\nRectangle created')
+print(rectangle.perimeter())
+print(rectangle.area())
